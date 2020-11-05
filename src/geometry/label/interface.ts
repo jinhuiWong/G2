cdfba2b44bdd9ae6997cf54b68d2bd0a8c4d2b9d
xinming <xinming.lxj@antfin.com>
2020-11-05 11:13:38 +0800

    refactor(typings): 类型定义调整

--- src/geometry/label/interface.ts
+++ src/geometry/label/interface.ts
@@ -1,6 +1,8 @@
 import { Coordinate, ShapeAttrs } from '../../dependents';
 import { Datum, GeometryLabelCfg, MappingDatum, Point } from '../../interface';
+
 export type TextAlign = 'start' | 'center' | 'end' | 'left' | 'right';
+
 export interface LabelCfg extends GeometryLabelCfg {
   content?: any;
   position?: 'top' | 'bottom' | 'middle' | 'left' | 'right';
@@ -40,22 +42,6 @@ export interface LabelItem extends GeometryLabelCfg {
   r?: number;
   /** 牵引线 */
   labelLine?: null | boolean | { style?: object; path?: string };
-
-  /**
-   * label 背景
-   */
-  background?: {
-    /**
-     * 背景框 图形属性配置
-     * - fill?: string; 背景框 填充色
-     * - stroke?: string; 背景框 描边色
-     * - lineWidth?: string; 背景框 描边宽度
-     * - radius?: number | number[]; 背景框圆角，支持整数或数组形式
-     */
-    style?: ShapeAttrs;
-    /** 背景框 内边距 */
-    padding?: number | number[];
-  };
 }
 
 /**
